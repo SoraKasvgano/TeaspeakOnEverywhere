841363226adc382cc514f5c96f50bbed603d35e6
Sora <8521327+SoraKasvgano@users.noreply.github.com>
2025-09-30 20:10:28 +0800

    1
    1

--- TeaSpeak-1.4.22/teastart_minimal.sh
+++ TeaSpeak-1.4.22/teastart_minimal.sh
@@ -4,7 +4,7 @@ file_base=$(readlink -f "$0")
 directory_base=$(dirname ${file_base})
 cd ${directory_base}
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH;./libs/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./libs/"
 export LD_PRELOAD="./libs/libjemalloc.so.2"
 
 ./TeaSpeakServer $@
